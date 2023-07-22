df5ce449289e65b139adfcd5e965d687749eb57b
Tom Forbes <tom@tomforb.es>
2023-07-22 20:38:08 +0100

    Try loading data like this?

--- sql/combine.sql
+++ sql/combine.sql
@@ -1,7 +1,4 @@
 PRAGMA memory_limit='2GB';
--- PRAGMA enable_progress_bar;
 PRAGMA threads=4;
-COPY (select *
-    from read_parquet('input/*.parquet', union_by_name=True)) TO temp_table;
-
+COPY temp_table FROM 'input/*.parquet' (FORMAT PARQUET);
 COPY temp_table TO 'output.parquet' (FORMAT PARQUET, COMPRESSION zstd);
