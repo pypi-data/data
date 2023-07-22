0685b89642ea076dc6396c75075644d2a3f1fe43
Tom Forbes <tom@tomforb.es>
2023-07-22 18:36:39 +0100

    ok

--- src/pypi_data/cli.py
+++ src/pypi_data/cli.py
@@ -1,18 +1,14 @@
 import json
-import sys
-import tempfile
 from pathlib import Path
 from typing import Annotated, Iterable
 from fsspec.implementations.http_sync import HTTPFileSystem
 import typer
-import shutil
 import polars as pl
 from github import Github
 from github import Auth
 import requests
 from concurrent.futures import ThreadPoolExecutor
 
-from tqdm import tqdm
 
 app = typer.Typer()
 session = requests.Session()
@@ -74,47 +70,47 @@ def group_index_urls(github_token: GithubToken,
         )
         # print(f"Group {idx} contains {len(paths)} paths", file=sys.stderr)
     print(json.dumps(outputs))
-
-
-def _download_file(url: str, to: Path):
-    with requests.get(url, stream=True) as r:
-        with to.open("wb") as f:
-            shutil.copyfileobj(r.raw, f, length=16 * 1024 * 1024)
-
-
-@app.command()
-def compact_indexes(github_token: GithubToken):
-    g = github_client(github_token)
-    repo = g.get_repo("pypi-data/data")
-    new_release = repo.create_git_release(
-        "latest", "latest", draft=True, target_commitish="main", message="latest data"
-    )
-    for idx, paths in enumerate(group_by_size(g, target_size=int(GB * 1.3))):
-        print(f"Downloading group {idx} containing {len(paths)} paths")
-        with tempfile.TemporaryDirectory() as tmpdir, ThreadPoolExecutor() as pool:
-            input_dir = Path(tmpdir) / "input"
-            output_dir = Path(tmpdir) / "output"
-            input_dir.mkdir()
-            output_dir.mkdir()
-            list(
-                pool.map(
-                    lambda r: _download_file(r[1], input_dir / f"{r[0]}.parquet"),
-                    enumerate(paths),
-                )
-            )
-            print(f"Downloaded group {idx}. Combining...")
-            combined = pl.scan_parquet(f"{input_dir}/*.parquet", low_memory=False)
-            combined.sink_parquet(
-                output_dir / f"{idx}.parquet",
-                compression="zstd",
-                compression_level=14,
-                statistics=True,
-                maintain_order=False,
-                row_group_size=64 * MB,
-                data_pagesize_limit=1 * MB,
-            )
-            new_release.upload_asset(str(output_dir / f"{idx}.parquet"))
-            print(f"Downloaded group {idx}")
+#
+#
+# def _download_file(url: str, to: Path):
+#     with requests.get(url, stream=True) as r:
+#         with to.open("wb") as f:
+#             shutil.copyfileobj(r.raw, f, length=16 * 1024 * 1024)
+#
+#
+# @app.command()
+# def compact_indexes(github_token: GithubToken):
+#     g = github_client(github_token)
+#     repo = g.get_repo("pypi-data/data")
+#     new_release = repo.create_git_release(
+#         "latest", "latest", draft=True, target_commitish="main", message="latest data"
+#     )
+#     for idx, paths in enumerate(group_by_size(g, target_size=int(GB * 1.3))):
+#         print(f"Downloading group {idx} containing {len(paths)} paths")
+#         with tempfile.TemporaryDirectory() as tmpdir, ThreadPoolExecutor() as pool:
+#             input_dir = Path(tmpdir) / "input"
+#             output_dir = Path(tmpdir) / "output"
+#             input_dir.mkdir()
+#             output_dir.mkdir()
+#             list(
+#                 pool.map(
+#                     lambda r: _download_file(r[1], input_dir / f"{r[0]}.parquet"),
+#                     enumerate(paths),
+#                 )
+#             )
+#             print(f"Downloaded group {idx}. Combining...")
+#             combined = pl.scan_parquet(f"{input_dir}/*.parquet", low_memory=False)
+#             combined.sink_parquet(
+#                 output_dir / f"{idx}.parquet",
+#                 compression="zstd",
+#                 compression_level=14,
+#                 statistics=True,
+#                 maintain_order=False,
+#                 row_group_size=64 * MB,
+#                 data_pagesize_limit=1 * MB,
+#             )
+#             new_release.upload_asset(str(output_dir / f"{idx}.parquet"))
+#             print(f"Downloaded group {idx}")
 
 
 if __name__ == "__main__":
