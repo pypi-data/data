edd44d28b0216cb7e4eb501b0274450c6f8fad12
Tom Forbes <tom@tomforb.es>
2023-07-22 18:12:45 +0100

    Try compating the indexes like this

--- src/pypi_data/cli.py
+++ src/pypi_data/cli.py
@@ -1,66 +1,105 @@
+import tempfile
 from pathlib import Path
 from typing import Annotated, Iterable
-import json
+from fsspec.implementations.http_sync import HTTPFileSystem
 import typer
+import shutil
 import polars as pl
 from github import Github
 from github import Auth
+import requests
 from concurrent.futures import ThreadPoolExecutor
 
 from tqdm import tqdm
 
 app = typer.Typer()
+session = requests.Session()
+MB = 1024 * 1024
+GB = MB * 1024
+
+GithubToken = Annotated[str, typer.Argument(envvar="GITHUB_TOKEN")]
 
 
-@app.command()
-def print_index_urls(github_token: Annotated[str, typer.Argument(envvar="GITHUB_TOKEN")]):
+def _get_index_urls(github: Github) -> Iterable[tuple[str]]:
+    for repo in github.get_organization("pypi-data").get_repos():
+        if repo.name.startswith("pypi-mirror-"):
+            yield f"{repo.html_url}/releases/download/latest/dataset.parquet"
+
+
+def github_client(github_token) -> Github:
     auth = Auth.Token(github_token)
     g = Github(auth=auth)
     g.per_page = 100
-    for repo in g.get_organization("pypi-data").get_repos():
-        if repo.name.startswith('pypi-mirror-'):
-            print(f'{repo.html_url}/releases/download/latest/dataset.parquet')
-            # print(json.dumps({
-            #     "url": f'{repo.html_url}/releases/download/latest/dataset.parquet',
-            #     "repo": repo.name,
-            # }))
+    return g
 
 
-def group_by_size(directory: Path, target_size: int) -> Iterable[list[Path]]:
-    names = []
-    total_size = 0
-    for path in directory.glob("*.parquet"):
-        names.append(path)
-        total_size += path.stat().st_size
-        if total_size >= target_size:
-            yield names
-            names.clear()
-            total_size = 0
-    yield names
+@app.command()
+def print_index_urls(github_token: GithubToken):
+    g = github_client(github_token)
+    for url in _get_index_urls(g):
+        print(url)
+
+
+def group_by_size(github: Github, target_size: int) -> Iterable[list[str]]:
+    fs = HTTPFileSystem()
+    urls = _get_index_urls(github)
+    with ThreadPoolExecutor() as pool:
+        stat_results = pool.map(lambda url: fs.stat(url), urls)
+
+        names = []
+        total_size = 0
+        for stat_result in stat_results:
+            names.append(stat_result["name"])
+            total_size += stat_result["size"]
+            if total_size >= target_size:
+                yield names
+                names.clear()
+                total_size = 0
+        yield names
+
+
+def _download_file(url: str, to: Path):
+    with requests.get(url, stream=True) as r:
+        with to.open("wb") as f:
+            shutil.copyfileobj(r.raw, f, length=16 * 1024 * 1024)
 
 
 @app.command()
 def compact_indexes(
-        directory: Annotated[Path, typer.Argument(dir_okay=True, file_okay=False, writable=True, resolve_path=True)],
-        output_dir: Annotated[Path, typer.Argument(dir_okay=True, file_okay=False, writable=True, resolve_path=True)],
+    github_token: GithubToken,
 ):
-    frames = []
-    for idx, paths in enumerate(group_by_size(directory, target_size=int(1024 * 1024 * 1024 * 1.3))):
-        frames.append(pl.concat([
-            pl.scan_parquet(path, low_memory=True)
-            for path in paths
-        ]))
+    g = github_client(github_token)
+    repo = g.get_repo("pypi-data/data")
+    new_release = repo.create_git_release(
+        "latest", "latest", draft=True, target_commitish="main", message="latest data"
+    )
+    for idx, paths in enumerate(group_by_size(g, target_size=int(GB * 1.3))):
+        print(f"Downloading group {idx} containing {len(paths)} paths")
+        with tempfile.TemporaryDirectory() as tmpdir, ThreadPoolExecutor() as pool:
+            input_dir = Path(tmpdir) / "input"
+            output_dir = Path(tmpdir) / "output"
+            input_dir.mkdir()
+            output_dir.mkdir()
+            list(
+                pool.map(
+                    lambda r: _download_file(r[1], input_dir / f"{r[0]}.parquet"),
+                    enumerate(paths),
+                )
+            )
+            print(f"Downloaded group {idx}. Combining...")
+            combined = pl.scan_parquet(f"{input_dir}/*.parquet", low_memory=False)
+            combined.sink_parquet(
+                output_dir / f"{idx}.parquet",
+                compression="zstd",
+                compression_level=14,
+                statistics=True,
+                maintain_order=False,
+                row_group_size=64 * MB,
+                data_pagesize_limit=1 * MB,
+            )
+            new_release.upload_asset(str(output_dir / f"{idx}.parquet"))
+            print(f"Downloaded group {idx}")
 
-    with ThreadPoolExecutor() as pool:
-        result = pool.map(lambda f: f[1].sink_parquet(
-            output_dir / f'{f[0]}.parquet',
-            compression='zstd',
-            compression_level=12,
-            statistics=True,
-            maintain_order=False,
-        ), enumerate(frames))
-        list(tqdm(result, total=len(frames)))
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app()
