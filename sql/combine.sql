f85f58d8bbcdf310e2707cb5e5c2cbbd279e9ad1
Tom Forbes <tom@tomforb.es>
2023-07-22 20:42:33 +0100

    Try this

--- sql/combine.sql
+++ sql/combine.sql
@@ -1,4 +1,4 @@
 PRAGMA memory_limit='2GB';
 PRAGMA threads=4;
-COPY temp_table FROM 'input/*.parquet' (FORMAT PARQUET);
+CREATE TABLE temp_table AS SELECT * FROM read_parquet('input/*.parquet', union_by_name=True);
 COPY temp_table TO 'output.parquet' (FORMAT PARQUET, COMPRESSION zstd);
