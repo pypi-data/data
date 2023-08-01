aaf54bda5879dd6ef7ae23f9df9f0f6502a36a49
Tom Forbes <tom@tomforb.es>
2023-08-01 20:05:45 +0100

    Update combine.sql

--- sql/combine.sql
+++ sql/combine.sql
@@ -3,5 +3,5 @@ PRAGMA threads=4;
 CREATE TABLE temp_table AS
 select regexp_extract(filename, '(\d+)\.parquet', 1)::USMALLINT as repository, * exclude (filename)
 FROM read_parquet('input/*.parquet', union_by_name = True, filename=true)
-order by (project_name, project_version, project_release, path);
+order by (path);
 COPY temp_table TO 'output.parquet' (FORMAT PARQUET, COMPRESSION zstd);
