e9b7d3563d100a121da3adac9c6607b00989df29
Tom Forbes <tom@tomforb.es>
2023-08-05 17:33:03 +0100

    Sort by archive path

--- sql/combine.sql
+++ sql/combine.sql
@@ -4,5 +4,5 @@ SET enable_progress_bar=true;
 CREATE TABLE temp_table AS
 select regexp_extract(filename, '(\d+)\.parquet', 1)::USMALLINT as repository, * exclude (filename)
 FROM read_parquet('input/*.parquet', union_by_name = True, filename = true)
-order by (uploaded_on, path);
+order by (archive_path);
 COPY temp_table TO 'output.parquet' (FORMAT PARQUET, COMPRESSION zstd);
