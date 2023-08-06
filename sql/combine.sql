bb6909d2e2adff92dd0d53233334ea0e14dfa074
Tom Forbes <tom@tomforb.es>
2023-08-06 15:01:27 +0100

    Use parquet CLI to combine

--- sql/combine.sql
+++
@@ -1,8 +0,0 @@
-PRAGMA memory_limit='2GB';
-PRAGMA threads=4;
-SET enable_progress_bar=true;
-CREATE TABLE temp_table AS
-select regexp_extract(filename, '(\d+)\.parquet', 1)::USMALLINT as repository, * exclude (filename)
-FROM read_parquet('input/*.parquet', union_by_name = True, filename = true)
-order by (archive_path);
-COPY temp_table TO 'output.parquet' (FORMAT PARQUET, COMPRESSION zstd);
